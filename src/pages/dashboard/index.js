209c102a862d08036b2445cdd17fa37994bf8455
DESKTOP-FL2RMF5\rnwk3 <rnwkfydwkd82@likelion.org>
2021-12-27 16:27:52 +0900

    Add 간단한 테이블 추가

--- src/pages/dashboard/index.js
+++ src/pages/dashboard/index.js
@@ -10,8 +10,46 @@ import {
 	Legend,
 	Bar,
 } from 'recharts';
+import './dash.scss';
 
 const Dashboard = () => {
+	const tempNoticeData = {
+		maxSize: 302,
+		currentPage: 1,
+		list: [
+			{
+				id: 1,
+				title: '치약',
+				date: '2021. 05. 23',
+			},
+			{
+				id: 2,
+				title: '게임',
+				date: '2021. 05. 23',
+			},
+			{
+				id: 3,
+				title: '칫솔',
+				date: '2021. 05. 23',
+			},
+			{
+				id: 4,
+				title: '젤다',
+				date: '2021. 05. 23',
+			},
+			{
+				id: 5,
+				title: '모바일',
+				date: '2021. 05. 23',
+			},
+			{
+				id: 6,
+				title: '프로그램 업데이트 안내',
+				date: '2021. 05. 23',
+			},
+		],
+	};
+
 	const data = [
 		{
 			name: 'Page A',
@@ -112,6 +150,26 @@ const Dashboard = () => {
 				<Bar dataKey="pv" fill="#8884d8" />
 				<Bar dataKey="uv" fill="#82ca9d" />
 			</BarChart>
+
+			<table className="dash">
+				<thead>
+					<tr>
+						<th>순위</th>
+						<th>검색어</th>
+					</tr>
+				</thead>
+
+				<tbody>
+					{tempNoticeData.list.map((i, index) => {
+						return (
+							<tr key={index + 'notice'}>
+								<td>{index}</td>
+								<td>{i.title}</td>
+							</tr>
+						);
+					})}
+				</tbody>
+			</table>
 		</div>
 	);
 };
