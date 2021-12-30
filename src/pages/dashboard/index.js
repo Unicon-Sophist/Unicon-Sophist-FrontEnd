96dc75e3ee112c6b4ec4f9ac1023f4861982b2a4
DESKTOP-FL2RMF5\rnwk3 <rnwkfydwkd82@likelion.org>
2021-12-30 21:03:57 +0900

    Fix 대시보드 css 수정

--- src/pages/dashboard/index.js
+++ src/pages/dashboard/index.js
@@ -133,7 +133,7 @@ const Dashboard = () => {
 		},
 	];
 	return (
-		<div className="">
+		<div className="dashboard">
 			<LineChart width={600} height={300} data={data}>
 				<Line type="monotone" dataKey="uv" stroke="#8884d8" />
 				<CartesianGrid stroke="#ccc" />
